d83e7dc4505655ae04bf14bd2f59366956ba941d
Custos <kutlenkov.dmitri@gmail.com>
2023-08-19 13:48:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import json
+import threading
+import time
+
+from websockets.sync.client import connect
+
+n_threads = 5
+
+
+def run_thread(n):
+    with connect("wss://fstream.binance.com/ws/btcusdt@bookTicker") as websocket:
+        with open(f'./out/stream_{n}.csv', 'w') as f:
+            f.write('arrival_time,event_time,updateId\n')
+            start_time = time.time()
+            current_time = start_time
+            while current_time - start_time <= 60:
+                message = websocket.recv()
+                current_time = time.time()
+                parsed = json.loads(message)
+                f.write(f"{current_time * 1000},{parsed['E']},{parsed['u']}\n")
+
+
+if __name__ == "__main__":
+    threads = []
+    for i in range(n_threads):
+        t = threading.Thread(target=run_thread, args=(i,))
+        t.start()
+        threads.append(t)
+    for i in range(n_threads):
+        threads[i].join()
